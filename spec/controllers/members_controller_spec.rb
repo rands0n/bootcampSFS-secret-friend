ed1b322a3b9b6f11ee3259980f6c573b68484d83
Randson Oliveira <orandson@gmail.com>
2018-02-07 00:33:41 -0400

    create members controller and specs for it

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -1,26 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe MembersController, type: :controller do
+  include Devise::Test::ControllerHelpers
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  let(:user) { create(:user) }
+  let(:campaign) { create(:campaign) }
+  let(:member) { create(:member, campaign_id: campaign.id) }
+  let(:member_attributes) { attributes_for(:member, campaign_id: campaign.id) }
+
+  before :each do
+    sign_in user
+  end
+
+  describe 'POST #create' do
+    context 'when member is saved' do
+      it 'redirects to home page' do
+        post :create, params: { member: member_attributes }
+
+        expect(response).to redirect_to root_url
+      end
+
+      it 'creates a campaign with the right attributes' do
+        post :create, params: { member: member_attributes }
+
+        expect(Member.last.name).to be_truthy
+        expect(Member.last.email).to be_truthy
+      end
+
+      it 'has the associated campaign' do
+        post :create, params: { member: member_attributes }
+
+        expect(Member.last.campaign.title).to be_truthy
+      end
     end
   end
 
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
+  describe 'DELETE #destroy' do
+    before :each do
+      request.env['HTTP_ACCEPT'] = 'application/json'
+    end
+
+    it 'returns http success' do
+      delete :destroy, params: { id: member.id }
+
+      expect(response).to have_http_status :success
     end
   end
 
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
+  describe 'PUT #update' do
+    context 'when updating the member' do
+      it 'returns http success' do
+        put :update, params: { id: member.id, member: member_attributes }
+
+        expect(response).to have_http_status :success
+      end
+
+      it 'then the attributes should be changed' do
+        put :update, params: { id: member.id, member: member_attributes }
+
+        expect(member.name).to be_truthy
+      end
     end
   end
-
 end
